bcca44f42f576f1aaf9ff1e3b3cc5db6de2b8386
Alex Alonso <alex.xmde@gmail.com>
2016-04-19 19:59:05 -0300

    Fixed Tipo

--- core/server/index.js
+++ core/server/index.js
@@ -5,7 +5,7 @@ var ExpressApp = require('./config/express');
 function Server() {
 	this.start = function(callback) {
 	port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-	ip = process.env.OPENSFHIT_NODEJS_IP || '127.0.0.1';
+	ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 		//Starts server at configurated port.
 		ExpressApp.listen(ExpressApp.get('port'), ip, function(error){
